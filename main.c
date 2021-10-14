9a466758192bde26688f4895e37d11e1ec3ba9f7
Hocde Lucas <hocdelucas+git@gmail.com>
2021-10-14 15:50:24 +0200

    Test de toutes les fonctions relatives au stack: OK

--- main.c
+++ main.c
@@ -19,7 +19,7 @@
 #include "stack.h"
 
 
-int main(int argc, char** argv) {
+int main() {
     Stack *stack;
     float ret_pop;
 
@@ -29,21 +29,35 @@ int main(int argc, char** argv) {
 
     push(stack,7);
     push(stack,1723);
+    printf("Peek 1723: %.0f\n",peek(stack));
     push(stack,-657);
+    printf("Peek -657: %.0f\n",peek(stack));
 
-    //ret_pop = pop(stack);
     ret_pop = pop(stack);
-    //ret_pop = pop(stack);
-
-    swap(stack);
-
-    dup(stack);
-
-   
-
     printf("ret_pop: %.0f\n",ret_pop);
 
+    swap(stack);
+    printf("Peek 7: %.0f\n",peek(stack));
+
+    dup(stack);
+   
     printf("stack stat: %d\n",is_stack_empty(stack));
+    printf("popping: %.0f\n",pop(stack));
+    printf("popping: %.0f\n",pop(stack));
+    printf("popping: %.0f\n",pop(stack));
+    printf("stack stat: %d\n",is_stack_empty(stack));
+
+    printf("restacking...\n");
+
+    push(stack, 1111);
+    push(stack, 69420);
+    push(stack, 3.1415);
+    printf("stack stat: %d\n",is_stack_empty(stack));
+    printf("reseting...\n");
+    stack_clear(stack);
+    printf("stack stat: %d\n",is_stack_empty(stack));
+
+    
 
     return (EXIT_SUCCESS);
 }
