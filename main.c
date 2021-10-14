9752867703206eeb1de2980cfbe8355c497dfa10
Hocde Lucas <hocdelucas+git@gmail.com>
2021-10-14 17:46:28 +0200

    Tests Queue

--- main.c
+++ main.c
@@ -17,13 +17,20 @@
 #include <assert.h>
 
 #include "stack.h"
+#include "queue.h"
 
 
 int main() {
+
+    Queue *queue;
     Stack *stack;
     float ret_pop;
 
     stack = malloc(sizeof(Stack));
+    queue = malloc(sizeof(queue));
+
+    printf("====================STACK DEBUT====================\n");
+    
 
     init_stack(stack);
 
@@ -32,7 +39,7 @@ int main() {
     printf("Peek 1723: %.0f\n",peek(stack));
     push(stack,-657);
     printf("Peek -657: %.0f\n",peek(stack));
-
+    
     ret_pop = pop(stack);
     printf("ret_pop: %.0f\n",ret_pop);
 
@@ -57,7 +64,32 @@ int main() {
     stack_clear(stack);
     printf("stack stat: %d\n",is_stack_empty(stack));
 
+    printf("====================STACK FIN====================\n");
+
+
+    printf("====================QUEUE DEBUT====================\n");
+
+    enqueue(queue, 27);
+    enqueue(queue, 157);
+    enqueue(queue, 9999);
+    enqueue(queue, -3612);
+    enqueue(queue, 90);
     
+    printf("Dequeue: %.0f\n",dequeue(queue));
+    printf("Dequeue: %.0f\n",dequeue(queue));
+    printf("Dequeue: %.0f\n",dequeue(queue));
+    
+    printf("Queue empty : %d\n",is_queue_empty(queue));    
+
+
+    printf("Front: %.0f\n", front(queue));
+
+    printf("Dequeue: %.0f\n",dequeue(queue));
+    printf("Dequeue: %.0f\n",dequeue(queue));
+
+    printf("Queue empty : %d\n",is_queue_empty(queue));
+
+    printf("====================QUEUE FIN====================\n");
 
     return (EXIT_SUCCESS);
 }
